a5ccac31c892cd77d7e86bb6157c967d11bfffbe
Andrey Vasilyev <118031542+Andrey199123@users.noreply.github.com>
2024-12-01 21:15:30 -0500

    Fixed deprecated urls library

--- django-polls/homepage/urls.py
+++ django-polls/homepage/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 from . import views
+
 app_name = 'homepage'
+
 urlpatterns = [
-    url('', views.home, name='homepage'),
-    url('secret', views.secret, name='secret'),
-    url("static/homepage/other/favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("{% static 'homepage/other/favicon.ico' %}")))
+    path('', views.home, name='homepage'),
+    path('secret', views.secret, name='secret'),
+    re_path(r"^static/homepage/other/favicon.ico$", RedirectView.as_view(url=staticfiles_storage.url("homepage/other/favicon.ico")))
 ]
